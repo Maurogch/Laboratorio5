3bf7c305155d59a90a2e93788eb147311cc58e62
maurogch <maurogch@gmail.com>
2019-05-18 03:14:12 -0300

    Spring web-Thymeleaf example

---
+++ SpringWebHello/src/main/java/hello/GreetingController.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    //RequestParam is stating that the variable name can be asigned in get (?name="unNombre"), que no es necesaria
+    //y que por defecto sera World
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
